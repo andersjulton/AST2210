8b1e58fe66df66c4299401df7b3fd8551a5aab0a
helland <helleland8@hotmail.com>
2017-11-22 14:23:07 +0100

    combined plots

--- COBE-DMR/plot_contours.py
+++ COBE-DMR/plot_contours.py
@@ -18,6 +18,13 @@ if __name__ == "__main__":
         n_values = a[1,:]
         lnL_init = a[2:,:]
         qgrid, ngrid = np.meshgrid(Q_values,n_values, indexing='ij')
+        
+        #copy past all code to get a second data set for maximum laziness
+        a2 = np.load("cobe_dmr_90ghz_lnL.npy")
+        Q_values2 = a2[0,:]
+        n_values2 = a2[1,:]
+        lnL_init2 = a2[2:,:]
+        qgrid2, ngrid2 = np.meshgrid(Q_values2,n_values2, indexing='ij')
 
     else: # ascii file
         n_Q = int(sys.argv[2])
@@ -30,20 +37,25 @@ if __name__ == "__main__":
         n_values = ngrid[0,:]
     #lnL = np.zeros_like(lnL_init)
     lnL = lnL_init - np.amax(lnL_init) # arbitrarily "normalizing" to make the numbers more manageable
+    lnL2 = lnL_init2 - np.amax(lnL_init2) # arbitrarily "normalizing" to make the numbers more manageable
 
     # For a Gaussian distribution, the 1, 2 and 3 sigma (68%, 95% and
     # 99.7%) confidence regions correspond to where -2 lnL increases by
     # 2.3, 6.17 and 11.8 from its minimum value. 0.1 is close to the peak. 
     my_levels = [0.1, 2.3, 6.17, 11.8]
-    cs = plt.contour(qgrid,ngrid, -2.*lnL, levels=my_levels) #, colors='k'
+    cs = plt.contour(qgrid,ngrid, -2.*lnL, levels=my_levels, colors='k') #
+    cs2 = plt.contour(qgrid2,ngrid2, -2.*lnL2, '--', levels=my_levels, colors='0.6', linestyles= 'dashed') #
     plt.grid()
     plt.xlabel("Q") 
     plt.ylabel("n",rotation=0) 
     plt.title("")
-    labels = ["Peak", "3 Sigma", "2 Sigma", "1 Sigma"]
-    for i in range(len(labels)): 
-        cs.collections[i].set_label(labels[i])
-    plt.legend(loc='upper right')
+    labels = ["53 GHz","90 GHz"]
+    cs.collections[0].set_label(labels[0])
+    cs2.collections[0].set_label(labels[1])
+        #labels = ["Peak", "1 $\sigma$", "2 $\sigma$","3 $\sigma$"]
+    #for i in range(len(labels)): 
+    #    cs.collections[i].set_label(labels[i])
+    plt.legend(loc='upper right', fontsize = 40)
     plt.show()
 
 
@@ -53,20 +65,31 @@ if __name__ == "__main__":
     P = np.exp(lnL_init-np.max(lnL_init))  
     dn = n_values[1] - n_values[0] #(n_values[len(n_values)-1] - n_values[0])/len(n_values)
     dQ = Q_values[1] - Q_values[0] #(Q_values[1] - Q_values[0])/len(Q_values)
+ 
+    P2 = np.exp(lnL_init2-np.max(lnL_init2))  
+    dn2 = n_values2[1] - n_values2[0] #(n_values[len(n_values)-1] - n_values[0])/len(n_values)
+    dQ2 = Q_values2[1] - Q_values2[0] #(Q_values[1] - Q_values[0])/len(Q_values)
     
     # Normalize to unit integral    
     P = P / (np.sum(P)*dn*dQ)    
+    P2 = P2 / (np.sum(P2)*dn2*dQ2)    
     
     # Compute marginal distribution, P(n|d) 
     P_n = np.zeros(len(n_values))
     P_Q = np.zeros(len(Q_values))
+    P_n2 = np.zeros(len(n_values2))
+    P_Q2 = np.zeros(len(Q_values2))
     for i in range(0, len(n_values)):
         P_n[i] = np.sum(P[:,i])  
         P_Q[i] = np.sum(P[i,:])
-    
+        P_n2[i] = np.sum(P2[:,i])  
+        P_Q2[i] = np.sum(P2[i,:])
+      
     # We now normalize 
     P_n = P_n / (sum(P_n) * dn)  
     P_Q = P_Q / (sum(P_Q) * dQ)  
+    P_n2 = P_n2 / (sum(P_n2) * dn2)  
+    P_Q2 = P_Q2 / (sum(P_Q2) * dQ2)  
     
     # We can now compute the mean mu and  the uncertainty sigma
     mu_n = sum(P_n * n_values)*dn                    
@@ -77,16 +100,26 @@ if __name__ == "__main__":
     mu_Q = sum(P_Q * Q_values)*dQ  
     sigma_Q = np.sqrt(sum(P_Q * (Q_values-mu_Q)**2)*dQ) 
     print "   mu_Q =",mu_Q
-    print "sigma_Q =",sigma_Q
+    print "sigma_Q =",sigma_Q,'\n\n-------\n'
+
+    mu_n2 = sum(P_n2 * n_values2)*dn2                    
+    sigma_n2 = np.sqrt(sum(P_n2*(n_values2-mu_n2)**2)*dn2)                 
+    print "   mu_n (90 GHz) =",mu_n2  
+    print "sigma_n (90 GHz) =",sigma_n2,"\n"
     
-    plt.plot(n_values,P_n)
+    mu_Q2 = sum(P_Q2 * Q_values2)*dQ2  
+    sigma_Q2 = np.sqrt(sum(P_Q2 * (Q_values2-mu_Q2)**2)*dQ2) 
+    print "   mu_Q (90 GHz) =",mu_Q2
+    print "sigma_Q (90 GHz) =",sigma_Q2
+        
+    plt.plot(n_values,P_n,n_values2,P_n2)
     plt.xlabel("n") 
     plt.ylabel("P(n)") 
     plt.axis([-1,3, min(P_n), max(P_n)*1.1])
-    plt.legend(["P(n)"]) 
+    plt.legend(["53 GHz", "90 GHz"]) 
     plt.show() 
-    plt.plot(Q_values,P_Q) 
+    plt.plot(Q_values,P_Q,Q_values2,P_Q2) 
     plt.axis([0,50,min(P_Q), max(P_Q)*1.1])  
     plt.ylabel("P(Q)")  
-    plt.legend(["P(Q)"]) 
+    plt.legend(["53 GHz", "90 GHz"]) 
     plt.show() 
